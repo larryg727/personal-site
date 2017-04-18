98dbdcb521eff17225530b49dc6c34ff9e4e4528
larryg727 <larrygonzales727@gmail.com>
2017-04-18 09:11:31 -0500

    -added jQuery effects on resume page. slidedown and fadeout

--- js/resume.js
+++ js/resume.js
@@ -1,24 +1,41 @@
+
 /**
  * Created by larryg on 4/17/17.
  */
-"use strict";
-(function () {
-
-var skills = document.getElementById("skills");
-
-var skills_content = document.getElementById("skills_content");
-
-var skillDropDown = function(){
-    skills_content.style.visibility = "visible";
-};
-skills.addEventListener("click", skillDropDown);
-
-var education = document.getElementById("education");
-var edu_content = document.getElementById("edu_content");
-var eduDropDown = function(){
-    edu_content.style.visibility = "visible";
-};
-
-education.addEventListener("click", eduDropDown);
-
-})();
+// "use strict";
+// (function () {
+//
+// var skills = document.getElementById("skills");
+//
+// var skills_content = document.getElementById("skills_content");
+//
+// var skillDropDown = function(){
+//     skills_content.slideDown();
+// };
+// skills.addEventListener("click", skillDropDown);
+//
+// var education = document.getElementById("education");
+// var edu_content = document.getElementById("edu_content");
+// var eduDropDown = function(){
+//     edu_content.style.visibility = "visible";
+// };
+//
+// education.addEventListener("click", eduDropDown);
+//
+// })();
+//
+// $(document).ready(function(){
+//     $("#education").click(function(){
+//         $("#edu_content").slideDown("slow");
+//     });
+// });
+$(document).ready(function(){
+    $("#education").click(function(){
+        $("#edu_content").slideDown("slow");
+        $(".eduArrow").fadeOut("slow");
+    });
+    $("#skills").click(function(){
+        $("#skills_content").slideDown("slow");
+        $(".skillArrow").fadeOut("slow");
+    });
+});
