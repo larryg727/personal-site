1dad134fb1143c39bc0a1eb28986016a7da96af7
larryg727 <larrygonzales727@gmail.com>
2017-05-03 10:35:23 -0500

    -updated home carousel with examples of work

--- js/resume.js
+++ js/resume.js
@@ -2,33 +2,7 @@
 /**
  * Created by larryg on 4/17/17.
  */
-// "use strict";
-// (function () {
-//
-// var skills = document.getElementById("skills");
-//
-// var skills_content = document.getElementById("skills_content");
-//
-// var skillDropDown = function(){
-//     skills_content.slideDown();
-// };
-// skills.addEventListener("click", skillDropDown);
-//
-// var education = document.getElementById("education");
-// var edu_content = document.getElementById("edu_content");
-// var eduDropDown = function(){
-//     edu_content.style.visibility = "visible";
-// };
-//
-// education.addEventListener("click", eduDropDown);
-//
-// })();
-//
-// $(document).ready(function(){
-//     $("#education").click(function(){
-//         $("#edu_content").slideDown("slow");
-//     });
-// });
+
 $(document).ready(function(){
     $("#education").click(function(){
         $("#edu_content").slideDown("slow");
