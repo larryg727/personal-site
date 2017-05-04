c6e7b0895536217a03fd5eaf48ac730d23511d33
larryg727 <larrygonzales727@gmail.com>
2017-05-04 11:16:48 -0500

    -added contact form to contact page

--- js/resume.js
+++ js/resume.js
@@ -8,10 +8,13 @@ $(document).ready(function(){
     $("#education").click(function(){
         $("#edu_content").slideToggle("slow");
         $(".arrowDown", this).fadeToggle("slow");
+        $(this).children().first().toggleClass("unactive");
+
     });
 
     $("#skills").click(function(){
         $("#skills_content").slideToggle("slow");
         $(".arrowDown", this).fadeToggle("slow");
+        $(this).children().first().toggleClass("unactive");
     });
 });
